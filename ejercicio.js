79fff028f77f979686ab2d3e5c29f6b9806786d0
Cristina <xispymoi@gmail.com>
2019-08-31 10:18:03 +0200

    Update ejercicio.js

--- ejercicio.js
+++ ejercicio.js
@@ -1,5 +1,13 @@
-var nombreYapellidos= Cristina Sánchez Álvarez
+var nombreYapellidos= "Cristina Sánchez Álvarez"
+
 var edad= 31
-var dias_de_la_semana= lunes martes miercoles jueves viernes sabado domingo
-var valor_de_la_gravedad= 9,807 m/s²
-var primeros_numeros_impares=1 3 5
+
+
+
+var dias_de_la_semana = new Array();
+var dias_de_la_semana = new Array(7);
+var dias_de_la_semana = new Array("lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo");
+
+var valor_de_la_gravedad = "9,807 m/s²"
+
+var primeros_numeros_impares = [1,3,5];
